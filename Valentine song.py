d59d3c05d3dbbe1abb3933ba88e69bd596f0bb24
jyotsnaagrawal <122946262+jyotsnaagrawal@users.noreply.github.com>
2023-01-28 23:13:38 -0600

    Add files via upload

---
+++ Valentine song.py
@@ -0,0 +1,37 @@
+# printing preschool valentine Song :
+# Mad_libs game using the valentine song "Love {person}, yes I {verb}"
+person = input("Enter the name of person name:")
+animal = input("Enter a type of animal:")
+food = input("Enter a food name:")
+noun_1 = input("Enter a noun_1:")
+noun_2 = input("Enter a noun_2:")
+noun_3 = input("Enter a noun_3:")
+noun_4 = input("Enter a noun_4:")
+feeling = input("Enter you feeling:")
+group_1 = input("Enter a group of people_1:")
+group_2 = input("Enter a group of people_2:")
+profession = input("Enter a profession:")
+verb = input("Enter a action:")
+adjective = input("Enter an adjective:")
+
+# substitute the words into the poem
+print(f"{feeling} {person}, yes I do")
+print(f"love {animal}, yes I do")
+print(f"{feeling} {profession}, yes I do")
+print(f"{feeling} {person}, but I won't tell who!\n")  # the \n leaves a blank line after this:
+
+print(f"{noun_1} like {food}, yes I will")
+print(f"{noun_2} like {food}, yes I will")
+print(f"{noun_3} like {food}, yes I will")
+print(f"{noun_4} like {food}, cause I can't {verb} still\n")
+
+print(f"{verb} a ladder, oh so {adjective}")
+print(f"{verb} a {noun_2}, oh so high")
+print(f"{verb} a ladder, oh so {adjective}")
+print(f"{verb} a {noun_1}, reach for the sky!")
+
+print(f"Love {group_1}, yes I do")
+print(f"Love {group_2}, yes I do")
+print(f"Kiss and a hug ,cause I {feeling} you!")
+
+
