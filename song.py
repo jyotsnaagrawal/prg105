95cbafe5b00baf5e0baf5a63e7a7501cde12c7c0
jyotsnaagrawal <122946262+jyotsnaagrawal@users.noreply.github.com>
2023-02-11 16:40:13 -0600

    Add files via upload

---
+++ song.py
@@ -0,0 +1,24 @@
+bottles = 99
+while bottles > 2:
+    print(str(bottles) + "bottles of beer on the wall")
+    print(f"{bottles} bottles of beer on the wall ")
+    print("Take one down, pass it around")
+    print(str(bottles - 1) + "bottles of beer on the wall\n")
+    bottles = bottles - 1
+
+    print(str(bottles) + "bottles of beer on the wall")
+    print(str(bottles) + "bottles of beer on the wall ")
+    print("Take one down, pass it around")
+    print(str(bottles - 1) + "bottle of beer on the wall\n")
+    bottles = bottles - 1
+
+    print(str(bottles) + "bottle of beer on the wall")
+    print(str(bottles) + "bottle of beer on the wall ")
+    print(str(bottles) + "Take one down, pass it around")
+    print(str(bottles - 1) + "bottle of beer on the wall\n")
+    bottles = bottles - 1
+
+    print(f"{bottles} bottle of beer on the wall")
+
+
+
