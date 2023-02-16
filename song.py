4106da6cb646bf817941d133818f1e3ed35456e1
jyotsnaagrawal <122946262+jyotsnaagrawal@users.noreply.github.com>
2023-02-16 10:35:42 -0600

    Update song.py

--- song.py
+++ song.py
@@ -1,22 +1,26 @@
 bottles = 99
 while bottles > 2:
-    print(str(bottles) + "bottles of beer on the wall")
-    print(f"{bottles} bottles of beer on the wall ")
-    print("Take one down, pass it around")
-    print(str(bottles - 1) + "bottles of beer on the wall\n")
+    print(str(bottles) + " bottles of beer on the wall,")
+    print(str(bottles) + " bottles of beer")
+    print(" Take one down, pass it around")
+    print(str(bottles - 1) + " bottles of beer on the wall\n")
     bottles = bottles - 1
 
-    print(str(bottles) + "bottles of beer on the wall")
-    print(str(bottles) + "bottles of beer on the wall ")
-    print("Take one down, pass it around")
-    print(str(bottles - 1) + "bottle of beer on the wall\n")
-    bottles = bottles - 1
+print(str(bottles) + " bottles of beer on the wall")
+print(str(bottles) + " bottles of beer ")
+print(" Take one down, pass it around")
+print(str(bottles - 1) + " bottle of beer on the wall\n\n")
+bottles -= 1
 
-    print(str(bottles) + "bottle of beer on the wall")
-    print(str(bottles) + "bottle of beer on the wall ")
-    print(str(bottles) + "Take one down, pass it around")
-    print(str(bottles - 1) + "bottle of beer on the wall\n")
-    bottles = bottles - 1
+print(str(bottles) + " bottle of beer on the wall")
+print(str(bottles) + " bottle of beer ")
+print(str(bottles) + " Take one down, pass it around")
+bottles -= 1
+print(str(bottles) + " bottles of beer on the wall")
+
+
+
+
 
    
 
