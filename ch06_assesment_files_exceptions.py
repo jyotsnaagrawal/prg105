2dcf1758d98e676849daf2f43a487b32a83f2457
jyotsnaagrawal <122946262+jyotsnaagrawal@users.noreply.github.com>
2023-02-27 09:21:03 -0600

    Add files via upload

---
+++ ch06_assesment_files_exceptions.py
@@ -0,0 +1,34 @@
+def main():
+    total = 0.0
+    count = 0
+    print("This program will total and average number in your data file. ")
+    file_name = input("Enter the name of your datafile: ")
+
+    try:
+        sales_file = open(file_name, "r")
+
+        line = sales_file.readline()
+        while line != "":
+            try:
+                value = float(line)
+                print(f"{value:,.2f}")
+                total += value
+                count += 1
+            except ValueError:
+                print(f'Line {count+1} with a value of {line} was invalid.')
+
+            line = sales_file.readline()
+
+        sales_file.close()
+
+    except IOError:
+        print(f'Unable to access the file: {file_name}.')
+        quit()
+
+    average = total / count
+    print(f"\nTotal:.........{total:,.2f}")
+    print(f"Number of entries:.....{count}")
+    print(f"Average:........{average:,.2f}")
+
+
+main()
