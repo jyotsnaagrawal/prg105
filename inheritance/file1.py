c0a59b025d57024e1227c2a765785d2b610cffc2
jyotsnaagrawal <122946262+jyotsnaagrawal@users.noreply.github.com>
2023-04-10 02:41:15 -0500

    Update file1.py

--- inheritance/file1.py
+++ inheritance/file1.py
@@ -35,39 +35,3 @@ class ProductionWorker(Employee):
     def get__hourly_pay_rate(self):
         return self.__hourly_pay_rate
 
-"""
-class Employee:
-    def __init__(self, employee_name, employee_number):
-        self.__employee_name = employee_name
-        self.__employee_number = employee_number
-
-    def set_name(self, name):
-        self.__name = name
-
-    def set_number(self, number):
-        self.__number = number
-
-    def get_name(self):
-        return self.__name
-
-    def get_number(self):
-        return self.__number
-
-
-class ProductionWorker(Employee):
-    def __init__(self, name, number, shift, hourly_pay_rate):
-        super().__init__(name, number)
-        self.__shift = shift
-        self.__hourly_pay_rate = hourly_pay_rate
-
-    def set_shift(self, shift):
-        self.__shift = shift
-
-    def set_hourly_pay_rate(self, hourly_pay_rate):
-        self.__hourly_pay_rate = hourly_pay_rate
-
-    def get_shift(self):
-        return self.__shift
-
-    def get_hourly_pay_rate(self):
-        return self.__hourly_pay_rate"""
