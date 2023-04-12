2c8d7f6cf0f9525a792dee0fecb83bc2b3aecd7d
jyotsnaagrawal <122946262+jyotsnaagrawal@users.noreply.github.com>
2023-04-12 17:16:47 -0500

    Update main.py

--- chap11_office_furnitures/main.py
+++ chap11_office_furnitures/main.py
@@ -5,9 +5,10 @@ from office_furniture import OfficeFurniture
 def main():
 
     office_furniture = OfficeFurniture("Desk", "Wood", 20, 50, 33, 100)
-    desk = Desk("Filing_Cabinet", "Iron", 56, 48, 56, 300, "Right", 6)
+    desk = Desk("Wood", 56, 48, 56, 300, "Right", 6)
     print(office_furniture)
     print(desk)
 
 
 main()
+
