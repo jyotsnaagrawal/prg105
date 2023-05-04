7f4fde6080a493d723187d3e0ad648f0b1cf7796
jyotsnaagrawal <122946262+jyotsnaagrawal@users.noreply.github.com>
2023-05-03 22:52:33 -0500

    Add files via upload

--- coffee_supplies.py
+++ coffee_supplies.py
@@ -1,18 +1,43 @@
+"""Using the coffee database created in the previous assignment as your data source, generate a report showing coffee
+supplies by category similar to the one below. Each category name should appear only once and categories should be in
+alphabetical order. (NOTE: Alphabetizing the products in each category is optional.)"""
+
 import sqlite3
 
-# Connect to the database
-conn = sqlite3.connect('coffee.db')
 
-# Retrieve data from the database
-cursor = conn.execute('SELECT Category, Product, Supplier FROM Coffee ORDER BY Category, Product')
+def main():
+    coffee_db = None
+    # connect to coffee database
+    try:
+        coffee_db = sqlite3.connect("coffee.db")
+        coffee_cursor = coffee_db.cursor()
+        coffee_cursor.execute("""SELECT Category FROM Coffee""")
+        category_list = coffee_cursor.fetchall()
 
-# Print the report header
-print('CATEGORY'.ljust(15), 'PRODUCT'.ljust(35), 'SUPPLIER')
-print("-" * 15, " " + "-" * 34, " " + "-" * 24)
-# Print each row of data
-for row in cursor:
-    print(row[0].ljust(15), row[1].ljust(35), row[2])
+        # initialize list
+        unique_category = []
+        for category in category_list:
+            if category[0] not in unique_category:
+                unique_category.append(category[0])
+
+        print("CATEGORY         PRODUCT         SUPPLIER")
+        print("========   ===================  =============")
+
+        # sort the categories and loop through them
+        unique_category.sort()
+        for category in unique_category:
+            print(category)
+            coffee_cursor.execute("""SELECT * FROM Coffee WHERE Category == ?""", (category,))
+            product_list = coffee_cursor.fetchall()
+            # display details for each product in the category
+            for product in product_list:
+                print(f"         {product[1]:20}  {product[3]}")
+
+    except sqlite3.Error:
+        print("%SQL error encountered.")
+    finally:
+        if coffee_db is not None:
+            coffee_db.close()
 
 
-# Close the database connection
-conn.close()
+main()
